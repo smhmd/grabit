3aa3274ad245f94c2f71821f69810057fb018b31
smhmd <contact.simohamed@gmail.com>
2020-11-24 14:46:05 +0100

    (SignInModal) use onSubmit instead of onClick

--- src/pages/Home/SignInModal.jsx
+++ src/pages/Home/SignInModal.jsx
@@ -66,7 +66,14 @@ export default function SignInModal() {
       <h3 className="text-sm font-semibold">
         {isLogginIn ? 'Login as:' : 'Sign up:'}
       </h3>
-      <form className="space-y-1">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          authenticate();
+          push('/');
+        }}
+        className="space-y-1"
+      >
         <Route exact path="/login">
           <div className="flex space-x-4">
             <Radio
@@ -86,15 +93,7 @@ export default function SignInModal() {
           </div>
         </Route>
         <div className="pt-3 space-y-2">
-          <Button
-            type="submit"
-            onClick={(e) => {
-              e.preventDefault();
-              authenticate();
-              push('/');
-            }}
-            colors="text-white bg-blue-600 w-full"
-          >
+          <Button type="submit" colors="text-white bg-blue-600 w-full">
             <img
               src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACYAAAAmCAYAAACoPemuAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAJpSURBVHgB3ZhNS1VBHIf/59KN0pKLi6KFFb1sRDNM3AS1iCToI7Ro3Spo4QdwVxAZREGbMBe9rKIXCFr0RmDlKmpVtIiwAg2VIgvy7zOcc+So53hnxjlX8YFnjs6dmfu7c++dO3NE1iiReKCqrVx68Ti2405sSh6ewa/4AZ/gaBRFP6QsCBNhDw7hhNozjXewV0LDoAfxIf7RlfEIj0kIGOgc/tdwmBc3gE3iAx1reE/L4wFucw3VqvG0l81b3GIbaiPe0sbxEpesDlFOsPNc+sWPMRzEVziVjL8Ja7gPrxX0u8mScrpwVELtx7/qxyWsyDIs0/cXHinqVNF4SfBhQCyoM8Zr3JzXqU/9+IgtAYIZTqVts1N/VvwY5vMxLWE4o9kvAv+04T/1I/enhvpO/KRumAzdpn86Y31YFT++FNRfx73iRjXJMh/sqPgzU1B/SPzoMkUabIeEx/cdaDNFGmyrrB12myIN5vZjWi7Npqg4dhqKljKZ13BxI6p2iQNpsJ9SPu2W7SZMkQb7JuVjG+y7KdJgY1I+XZbtxk2RBntj2amq8c42a+5Ja3E7qjosn2MkO8ge/K1+1AqC+dI5P2N8aT5zeSarj9lgvjd/ZJeLy7L6XGGSdEGNxhvF++pOqLdyRIt2wDxwACfVjRDBZrEn239BQqbxHZer0ngu8tyjdVtpfBht1Iw9RrtNBA034AvLgVcS7Ll6nMZbcNhicN9gT7FZfKCjWen7cSpgMPNBtzru2QTswLvJoL7BzEHaLEfdEpok4A0cdwhmbtzdxsNa56QeIqC5G3QCL2jB3RrqB/Ekbpf1xBwwuCneKLqjfAAAAABJRU5ErkJggg=="
               alt=""
